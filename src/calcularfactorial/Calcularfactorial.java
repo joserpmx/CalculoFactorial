b4557cfabf48aad122ef584258248bd6c2250038
joserpmx <joserp@hotmail.com>
2015-02-11 22:47:23 -0700

    Agregando Proyecto calcula Factorial

---
+++ src/calcularfactorial/Calcularfactorial.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcularfactorial;
+
+/**
+ *
+ * @author Jose
+ */
+import java.util.Scanner;
+public class Calcularfactorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Inicio de algoritmo
+        // Declaracion de variables
+        int Contador=0; //Sirve para controlar el ciclo y guardar el numero que se quiere calcular el factorial
+        float Factorial=1; //Acumulador sirve para guardar el resultado
+        int Numero,Respuesta=1;
+        Scanner lector=new Scanner(System.in);
+        while(Respuesta==1)
+        {    
+            //Solicitar el numero al que se le calcula el factorial
+            System.out.print("A que numero desea calcularle el factorial: ");
+            Contador=lector.nextInt();
+            Numero=Contador;
+            while(Contador>=2)
+            {    
+                Factorial=Factorial*Contador;
+                Contador=Contador-1;
+            }
+            System.out.println("El Factorial de "+ Numero + " es: " + Factorial);
+            Factorial=1;
+            System.out.print("Quieres otro calculo 1.- Si, 2.- No: ");
+            Respuesta=lector.nextInt();
+        }    
+    }
+}
